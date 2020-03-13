90a7899eb091dce59d2d1d78b42f523c4c4c90b2
Arpit Mishra <42835173+arpit0891@users.noreply.github.com>
2020-03-13 17:38:20 +0530

    Update p001.py

--- p001.py
+++ p001.py
@@ -1,10 +1,3 @@
-# 
-# Solution to Project Euler problem 1
-# Copyright (c) Project Nayuki. All rights reserved.
-# 
-# https://www.nayuki.io/page/project-euler-solutions
-# https://github.com/nayuki/Project-Euler-solutions
-# 
 
 
 # Computers are fast, so we can implement this solution directly without any clever math.
