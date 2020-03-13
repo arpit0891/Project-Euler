904ac014fe3333f3c82afb8fb322fc0e407538d7
Arpit Mishra <42835173+arpit0891@users.noreply.github.com>
2020-03-13 17:43:42 +0530

    Update p009.py

--- p009.py
+++ p009.py
@@ -1,12 +1,3 @@
-# 
-# Solution to Project Euler problem 9
-# Copyright (c) Project Nayuki. All rights reserved.
-# 
-# https://www.nayuki.io/page/project-euler-solutions
-# https://github.com/nayuki/Project-Euler-solutions
-# 
-
-
 # Computers are fast, so we can implement a brute-force search to directly solve the problem.
 def compute():
 	PERIMETER = 1000
