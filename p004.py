166d4f9a52225e2b02bc0b8bd6e78d11d231c6a8
Arpit Mishra <42835173+arpit0891@users.noreply.github.com>
2020-03-13 17:41:11 +0530

    Update p004.py

--- p004.py
+++ p004.py
@@ -1,11 +1,3 @@
-# 
-# Solution to Project Euler problem 4
-# Copyright (c) Project Nayuki. All rights reserved.
-# 
-# https://www.nayuki.io/page/project-euler-solutions
-# https://github.com/nayuki/Project-Euler-solutions
-# 
-
 
 # Computers are fast, so we can implement this solution directly without any clever math.
 def compute():
