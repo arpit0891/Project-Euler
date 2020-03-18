843e2660f8a8a7b31e65075bd6afe65a0a5af903
Arpit Mishra <42835173+arpit0891@users.noreply.github.com>
2020-03-18 20:44:34 +0530

    Update p097.py

--- p097.py
+++ p097.py
@@ -1,12 +1,3 @@
-# 
-# Solution to Project Euler problem 97
-# Copyright (c) Project Nayuki. All rights reserved.
-# 
-# https://www.nayuki.io/page/project-euler-solutions
-# https://github.com/nayuki/Project-Euler-solutions
-# 
-
-
 def compute():
 	MOD = 10**10
 	ans = (28433 * pow(2, 7830457, MOD) + 1) % MOD
