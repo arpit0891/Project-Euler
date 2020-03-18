f8e6692d5da6569f5512d0d9c0f3188deda992f9
Arpit Mishra <42835173+arpit0891@users.noreply.github.com>
2020-03-18 20:42:30 +0530

    Update p096.py

--- p096.py
+++ p096.py
@@ -1,12 +1,3 @@
-# 
-# Solution to Project Euler problem 96
-# Copyright (c) Project Nayuki. All rights reserved.
-# 
-# https://www.nayuki.io/page/project-euler-solutions
-# https://github.com/nayuki/Project-Euler-solutions
-# 
-
-
 def compute():
 	def extract(sudoku):  # For example: extract([3, 9, 4, 1, ...]) = 394
 		return int("".join(map(str, sudoku[ : 3])))
