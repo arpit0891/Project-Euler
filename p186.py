854d40f8cba175cc6022fe281360c0a255b55220
Arpit Mishra <42835173+arpit0891@users.noreply.github.com>
2020-03-22 17:49:43 +0530

    Update p186.py

--- p186.py
+++ p186.py
@@ -1,12 +1,3 @@
-# 
-# Solution to Project Euler problem 186
-# Copyright (c) Project Nayuki. All rights reserved.
-# 
-# https://www.nayuki.io/page/project-euler-solutions
-# https://github.com/nayuki/Project-Euler-solutions
-# 
-
-
 def compute():
 	ds = DisjointSet(1000000)
 	rand = LfgRandom()
