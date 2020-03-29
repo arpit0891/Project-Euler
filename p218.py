fb826b07df79a2a2dc394cd5bdf9d18b1865222e
Arpit Mishra <42835173+arpit0891@users.noreply.github.com>
2020-03-29 23:18:51 +0530

    Update p218.py

--- p218.py
+++ p218.py
@@ -1,12 +1,3 @@
-# 
-# Solution to Project Euler problem 218
-# Copyright (c) Project Nayuki. All rights reserved.
-# 
-# https://www.nayuki.io/page/project-euler-solutions
-# https://github.com/nayuki/Project-Euler-solutions
-# 
-
-
 # == Primitive Pythagorean triples ==
 # 
 # Each valid pair generates a PPT:
